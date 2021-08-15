835457c40443549031258c3dd5baba1766939eb7
hyunwoo <babywhitebear2021@gmail.com>
2021-08-15 14:55:09 +0900

    오답노트까지 commit

--- 백준/210811_그래프및기본개념/4949_다시풀기_균형잡힌세상.py
+++ 백준/210811_그래프및기본개념/4949_다시풀기_균형잡힌세상.py
@@ -14,12 +14,13 @@ while 1:
                 true_flag = 0 
                 break 
         elif cha == ']': 
-            if stack and stack[-1] == '[': stack.pop() 
-        else: 
-            true_flag = 0 
-            break 
-        if string == '.': break 
-        print("yes" if true_flag and not(stack) else "no")
+            if stack and stack[-1] == '[': 
+                stack.pop() 
+            else: 
+                true_flag = 0 
+                break 
+    if string == '.': break 
+    print("yes" if true_flag and not(stack) else "no")
 
 
     
--- 백준/210811_그래프및기본개념/9375번_패션왕신현빈.py
+++ 백준/210811_그래프및기본개념/9375번_패션왕신현빈.py
@@ -1,8 +1,28 @@
 # 테스트 케이스 횟수 T
 T=int(input())
-dic=dict()
+
 for _ in range(T):
+    dic=dict()
 # 테스트 케이스마다 옷의 갯수 N
     N=int(input())
     for _ in range(N):
-        dic
+        a,b=input().split()
+        if dic.get(b):
+            dic[b]+=1
+        else:
+            dic[b]=1
+    # print(dic)
+    sum=1
+    for num in dic.values():
+        sum*=(num+1)
+
+    print(sum-1)
+# 2
+# 3
+# hat headgear
+# sunglasses eyewear
+# turban headgear
+# 3
+# mask face
+# sunglasses face
+# makeup face
